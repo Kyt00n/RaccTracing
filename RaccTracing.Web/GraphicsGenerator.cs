4a9c28a6e8cf36622819a74f77b9c8d3513d52df
Kytoon <eryk.olsza@gmail.com>
2024-11-01 01:50:26 +0100

    add ray and CameraSettings

--- RaccTracing.Web/GraphicsGenerator.cs
+++ RaccTracing.Web/GraphicsGenerator.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Microsoft.Extensions.Configuration;
 using RaccTracing.Application.Interfaces;
 using RaccTracing.Domain.Entities;
 
@@ -7,23 +8,47 @@ namespace RaccTracing.Web;
 public class GraphicsGenerator
 {
     private readonly IColorService _colorService;
+    private readonly IConfiguration _configuration;
 
-    public GraphicsGenerator(IColorService colorService)
+    public GraphicsGenerator(IColorService colorService, IConfiguration configuration)
     {
         _colorService = colorService;
+        _configuration = configuration;
     }
 
-    public void GenerateImage(string filePath, int imageWidth, int imageHeight)
+    public void GenerateImage(string filePath)
     {
-        StringBuilder sb = new();
-        sb.Append($"P3\n{imageWidth} {imageHeight}\n255\n");
+        //TODO: Refactor this to use a model and a mapper
+        var renderImageSetup = _configuration.GetSection("RenderImageSetup");
+        var imageWidth = renderImageSetup.GetValue<int>("ImageWidth");
+        var aspectRatio = renderImageSetup.GetValue<double>("AspectRatio");
+        var focalLength = renderImageSetup.GetValue<double>("FocalLength");
+        var viewportHeight = renderImageSetup.GetValue<double>("ViewPortHeight");
+        var cameraCenterSection = renderImageSetup.GetSection("CameraCenter");
+        var cameraCenter = new Point3(
+            cameraCenterSection.GetValue<double>("X"),
+            cameraCenterSection.GetValue<double>("Y"),
+            cameraCenterSection.GetValue<double>("Z")
+        );
 
-        for (int j = 0; j < imageHeight; j++)
+        var cameraSettings = new CameraSettings
         {
-            Console.WriteLine($"Scan lines remaining: {imageHeight - j}");
-            for (int i = 0; i < imageWidth; i++)
+            ImageWidth = imageWidth,
+            AspectRatio = aspectRatio,
+            FocalLength = focalLength,
+            ViewportHeight = viewportHeight,
+            CameraCenter = cameraCenter
+        };
+        //TODO: Move to a service
+        StringBuilder sb = new();
+        sb.Append($"P3\n{cameraSettings.ImageWidth} {cameraSettings.ImageHeight}\n255\n");
+
+        for (var j = 0; j < cameraSettings.ImageHeight; j++)
+        {
+            Console.WriteLine($"Scan lines remaining: {cameraSettings.ImageHeight - j}");
+            for (var i = 0; i < cameraSettings.ImageWidth; i++)
             {
-                var pixelColor = new Vec3((double)i / (imageWidth - 1), (double)j / (imageHeight - 1), 0.0);
+                var pixelColor = new Vec3((double)i / (imageWidth - 1), (double)j / (cameraSettings.ImageHeight - 1), 0.0);
                 _colorService.WriteColor(sb, pixelColor);
             }
         }
--- RaccTracing.Web/Program.cs
+++ RaccTracing.Web/Program.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using RaccTracing.Application.Interfaces;
 using RaccTracing.Infrastructure.Services;
 using RaccTracing.Web;
 
-const int imageWidth = 256;
-const int imageHeight = 256;
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
 
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IColorService, ColorService>()
+    .AddSingleton<IConfiguration>(configuration)
     .AddSingleton<GraphicsGenerator>()
     .BuildServiceProvider();
 
@@ -17,4 +21,4 @@ var currentDirectory = Directory.GetCurrentDirectory();
 var filePath = Path.Combine(currentDirectory, "image.ppm");
 
 
-graphicsGenerator?.GenerateImage(filePath, imageWidth, imageHeight);
+graphicsGenerator?.GenerateImage(filePath);
