15d2b43769877b151657979542f197336543640b
Kytoon <eryk.olsza@gmail.com>
2024-11-01 20:14:41 +0100

    add a sphere to rendered image

--- RaccTracing.Application/Interfaces/IColorService.cs
+++ RaccTracing.Application/Interfaces/IColorService.cs
@@ -6,4 +6,5 @@ namespace RaccTracing.Application.Interfaces;
 public interface IColorService
 {
     void WriteColor(StringBuilder output, Vec3 pixelColor);
+    Color RayColor(Ray r);
 }
--- RaccTracing.Domain/Entities/Ray.cs
+++ RaccTracing.Domain/Entities/Ray.cs
@@ -2,9 +2,9 @@ namespace RaccTracing.Domain.Entities;
 
 public class Ray(Point3 origin, Vec3 direction)
 {
-    private Point3 Origin { get; } = origin;
+    public Point3 Origin { get; } = origin;
 
-    private Vec3 Direction { get; } = direction;
+    public Vec3 Direction { get; } = direction;
 
     public Vec3 At(double t)
     {
